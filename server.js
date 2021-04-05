3e76e72b455bf53153ae128b45a8471fd8183678
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2021-04-05 12:31:32 -0400

    Added Voting strategy to server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -17,8 +18,45 @@ let pollResult = [
   'Emma'
 ];
 
+const submittedBallots = new Map();
+
+function compareCandidate(a, b) {
+    if (a[1] < b[1]) {
+        return 1;
+    }
+    if (a[1] > b[1]) {
+        return -1;
+    }
+    return 0;
+}
+
 function calculatePoll(aUserId, aBallot) {
-    pollResult = aBallot;
+    submittedBallots.set(aUserId, aBallot);
+    const ballotCount = new Map();
+
+    for(let [eachUser, eachBallot] of submittedBallots){
+        for(let i = 0; i < eachBallot.length; i++){
+            const eachCandidate = eachBallot[i];
+            const voteWeight = (0.5)**(i);    // weight =  ar^(n) where a = 1 and i = n - 1
+            if(ballotCount.has(eachCandidate)){
+                const newCandidateTotal = voteWeight + ballotCount.get(eachCandidate);
+                ballotCount.set(eachCandidate, newCandidateTotal);
+            } else {
+                ballotCount.set(eachCandidate, voteWeight);
+            }
+        }
+    }
+    console.log("Map of totals: ");
+    console.log(ballotCount);
+
+    const candidateTotals = Array.from(ballotCount.entries())
+        .sort(compareCandidate)
+        .map(eachCandidate => eachCandidate[0]);
+    
+    console.log("Final Ordering: ");
+    console.log(candidateTotals);
+
+    pollResult = candidateTotals;
 }
 
 io.on("connection", function(socket) {
