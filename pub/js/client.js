d261a7d0f90340e60e4a3b9039da68a986759fb9
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2021-04-08 09:43:14 -0400

    Add Drag and Drop Prototype
    - UI Only! Does not update the actual ballot data

--- pub/js/client.js
+++ pub/js/client.js
@@ -8,7 +8,12 @@ const vm = {
       page: 'status',
       pollResults: [],
       ballot: [],
-      candidate: ''
+      candidate: '',
+      draggingEle: null,
+      x: 0,
+      y: 0,
+      placeholder: null,
+      //isDraggingStarted: null,
     };
   },
   methods: {
@@ -34,6 +39,112 @@ const vm = {
       socket.emit('submitBallot', this.ballot);
       this.page = 'status';
     },
+    mouseDownHandler(anEvent) {
+      this.draggingEle = anEvent.target;
+  
+      // Calculate the mouse position
+      const rect = this.draggingEle.getBoundingClientRect();
+      this.x = anEvent.pageX - rect.left;
+      this.y = anEvent.pageY - rect.top;
+  
+      // Attach the listeners to `document`
+      document.addEventListener('mousemove', this.mouseMoveHandler);
+      document.addEventListener('mouseup', this.mouseUpHandler);
+    },
+    swap(nodeA, nodeB) {
+      const parentA = nodeA.parentNode;
+      const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
+  
+      // Move `nodeA` to before the `nodeB`
+      nodeB.parentNode.insertBefore(nodeA, nodeB);
+  
+      // Move `nodeB` to before the sibling of `nodeA`
+      parentA.insertBefore(nodeB, siblingA);
+    },
+    setUpPlaceholder(aPlaceholdee){
+        // Let the placeholder take the height of dragging element
+        // So the next element won't move up
+        this.placeholder = document.createElement('div');
+        this.placeholder.classList.add('placeholder');
+        this.draggingEle.parentNode.insertBefore(
+            this.placeholder,
+            this.draggingEle.nextSibling
+        );
+        // Set the placeholder's height
+        this.placeholder.style.height = `${aPlaceholdee.height}px`;
+    },
+    createMovableRectangle(anEvent){
+        const draggingRect = this.draggingEle.getBoundingClientRect();
+        // Set position for dragging element
+        this.draggingEle.style.position = 'absolute';
+        this.draggingEle.style.top = `${anEvent.pageY - this.y}px`; 
+        this.draggingEle.style.left = `${anEvent.pageX - this.x}px`;
+        return draggingRect
+    },
+    mouseMoveHandler(anEvent) {
+        const movingElement = this.createMovableRectangle(anEvent);
+
+        if (!this.placeholder) {
+            //this.isDraggingStarted = true;
+            this.setUpPlaceholder(movingElement)
+        }
+
+        const elementAbove = this.draggingEle.previousElementSibling;
+        const elementBelow = this.placeholder.nextElementSibling;
+
+        if (this.isAbove(this.draggingEle, elementAbove)){
+            // The current order    -> The new order
+            // prevEle              -> placeholder
+            // draggingEle          -> draggingEle
+            // placeholder          -> prevEle
+            this.swap(this.placeholder, this.draggingEle);
+            this.swap(this.placeholder, elementAbove);
+        } else if (this.isAbove(elementBelow, this.draggingEle)){
+            // The current order    -> The new order
+            // draggingEle          -> nextEle
+            // placeholder          -> placeholder
+            // nextEle              -> draggingEle
+            this.swap(elementBelow, this.placeholder);
+            this.swap(elementBelow, this.draggingEle);
+        }
+    },
+    removePlaceholder(){
+        if (this.placeholder){
+            this.placeholder.parentNode.removeChild(this.placeholder);
+        }
+        this.placeholder = null;
+    },
+    mouseUpHandler() {
+        this.removePlaceholder();
+
+        // Remove the position styles
+        this.draggingEle.style.removeProperty('top');
+        this.draggingEle.style.removeProperty('left');
+        this.draggingEle.style.removeProperty('position');
+
+        this.x = null;
+        this.y = null;
+        this.draggingEle = null;
+
+        // Remove the handlers of `mousemove` and `mouseup`
+        document.removeEventListener('mousemove', this.mouseMoveHandler);
+        document.removeEventListener('mouseup', this.mouseUpHandler);
+
+        this.updateList();
+    },
+    updateList(){
+        const elements = document.getElementById('list').querySelectorAll('.draggable')[0].innerText;
+    },
+    isAbove(nodeA, nodeB) {
+        if (nodeA != null && nodeB != null){
+            // Get the bounding rectangle of nodes
+            const rectA = nodeA.getBoundingClientRect();
+            const rectB = nodeB.getBoundingClientRect();
+
+            return (rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2);
+        }
+        return false;
+    },
   },
 };
 
@@ -41,5 +152,4 @@ socket.on('updateResults', function(serverResults) {
   app.updatePoll(serverResults);
 });
 
-// eslint-disable-next-line no-unused-vars
 const app = Vue.createApp(vm).mount('#main');
