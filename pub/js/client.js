e6c164f2a538a5bb95de7bded0fffdac4127a10d
Albert Robin Moriarty <19686802+armoriarty@users.noreply.github.com>
2021-04-11 11:52:26 -0400

    Add Multiple Polls Capability

--- pub/js/client.js
+++ pub/js/client.js
@@ -5,23 +5,27 @@ const socket = io();
 const vm = {
     data() {
         return {
-        page: 'status',
+        page: 'polling station',
+        polls: null,
+        newPoll: '',
+        currentPoll: null,
         pollResults: [],
+        candidates: [],
         ballot: [],
-        candidate: '',
-        // movingCandidate: '',
-        // draggingEle: null,
-        // x: 0,
-        // y: 0,
-        // isDragging: false,
-        // placeholderIndex: null,
+        newCandidate: '',
         };
     },
     methods: {
+        addPoll(){
+            if(this.isNotBlank(this.newPoll)){
+                socket.emit('proposePoll', this.newPoll);
+                this.newPoll = '';
+            }
+        },
         addCandidateToBallot() {
-            if (this.isNotBlank(this.candidate)) {
-                this.ballot.unshift(this.candidate);
-                this.candidate = '';
+            if (this.isNotBlank(this.newCandidate)) {
+                this.ballot.unshift(this.newCandidate);
+                this.newCandidate = '';
             }
         },
         move(aSourceIndex, aDestinationIndex) {
@@ -29,148 +33,37 @@ const vm = {
             this.ballot[aSourceIndex] = this.ballot[aDestinationIndex];
             this.ballot[aDestinationIndex] = sourceValue;
         },
-        updatePoll(newPoll){
-            this.pollResults = newPoll;
-            this.ballot = this.pollResults;
+        setPollList(aPollNameList){
+            this.polls = aPollNameList
+        },
+        getPoll(aPollName){
+            socket.emit('requestPoll', aPollName);
+            this.page = 'status';
+        },
+        updatePoll(aPoll){
+            this.currentPoll = aPoll.name;
+            this.pollResults = aPoll.results;
+            this.ballot = aPoll.results;
         },
         isNotBlank(aString) {
             return aString.length != 0 && aString.trim();
         },
         submitBallot(){
-            socket.emit('submitBallot', this.ballot);
+            socket.emit('submitBallot', this.currentPoll, this.ballot);
             this.page = 'status';
         },
-        // mouseDownHandler(anEvent, anIndex) {
-        //     this.draggingEle = anEvent.target;
-        //     this.placeholderIndex = anIndex;
-        //     console.log("Setting up placeholderIndex: " + this.placeholderIndex);
-        //     console.log("\t Text at Placeholder is: " + this.ballot[this.placeholderIndex]);
-        
-        //     // Calculate the mouse position
-        //     const rect = this.draggingEle.getBoundingClientRect();
-        //     this.x = anEvent.pageX - rect.left;
-        //     this.y = anEvent.pageY - rect.top;
-        
-        //     // Attach the listeners to `document`
-        //     document.addEventListener('mousemove', this.mouseMoveHandler);
-        //     document.addEventListener('mouseup', this.mouseUpHandler);
-        // },
-        // swap(nodeA, nodeB) {
-        //     const parentA = nodeA.parentNode;
-        //     const siblingA = nodeA.nextSibling === nodeB ? nodeA : nodeA.nextSibling;
-        
-        //     // Move `nodeA` to before the `nodeB`
-        //     nodeB.parentNode.insertBefore(nodeA, nodeB);
-        
-        //     // Move `nodeB` to before the sibling of `nodeA`
-        //     parentA.insertBefore(nodeB, siblingA);
-        // },
-        // setUpPlaceholder(aPlaceholdee, aPlaceholderIndex){
-        //     // Modify the Ballot Array instead and insert a Placeholder Element
-
-        //     // // Let the placeholder take the height of dragging element
-        //     // // So the next element won't move up
-            
-        //     // this.placeholder = document.createElement('div');
-        //     // this.placeholder.classList.add('placeholder');
-        //     // this.draggingEle.parentNode.insertBefore(
-        //     //     this.placeholder,
-        //     //     this.draggingEle.nextSibling
-        //     // );
-        //     // // Set the placeholder's height
-        //     // this.placeholder.style.height = `${aPlaceholdee.height}px`;
-        // },
-        // createMovableRectangle(anEvent){
-        //     const draggingRect = this.draggingEle.getBoundingClientRect();
-        //     // Set position for dragging element
-        //     this.draggingEle.style.position = 'absolute';
-        //     this.draggingEle.style.top = `${anEvent.pageY - this.y}px`; 
-        //     this.draggingEle.style.left = `${anEvent.pageX - this.x}px`;
-        //     return draggingRect
-        // },
-        // mouseMoveHandler(anEvent) {
-        //     const movingElement = this.createMovableRectangle(anEvent);
-        //     // console.log(anEvent);
-
-        //     if (!this.isDragging) {
-        //         this.isDragging = true;
-        //         console.log("Creating placeholder element at: " + this.placeholderIndex);
-        //         console.log("\tHas Text: " + this.ballot[this.placeholderIndex]);
-        //         this.movingCandidate = this.ballot[this.placeholderIndex];
-        //         this.ballot[this.placeholderIndex] = "";
-        //         //this.setUpPlaceholder(movingElement , anIndex)
-        //     }
-
-        //     const elementAbove = document.getElementById('list').querySelectorAll('.draggable')[this.placeholderIndex - 1]; //this.draggingEle.previousElementSibling;
-        //     const elementBelow = document.getElementById('list').querySelectorAll('.draggable')[this.placeholderIndex + 1]; //this.placeholder.nextElementSibling;
-
-        //     // console.log(elementAbove);
-        //     // console.log(elementBelow);
-
-        //     if (this.isAbove(this.draggingEle, elementAbove)){
-        //         // The current order    -> The new order
-        //         // prevEle              -> placeholder
-        //         // draggingEle          -> draggingEle
-        //         // placeholder          -> prevEle
-        //         this.move(this.placeholderIndex, this.placeholderIndex - 1);
-        //         this.placeholderIndex--;
-        //         // this.swap(this.placeholder, this.draggingEle);
-        //         // this.swap(this.placeholder, elementAbove);
-        //     } else if (this.isAbove(elementBelow, this.draggingEle)){
-        //         // The current order    -> The new order
-        //         // draggingEle          -> nextEle
-        //         // placeholder          -> placeholder
-        //         // nextEle              -> draggingEle
-        //         this.move(this.placeholderIndex, this.placeholderIndex + 1);
-        //         this.placeholderIndex++;
-        //         // this.placeholderIndex++;
-        //         // this.move(this.placeholderIndex + 1, this.placeholderIndex + 2);
-        //         // this.swap(elementBelow, this.placeholder);
-        //         // this.swap(elementBelow, this.draggingEle);
-        //     }
-        // },
-        // removePlaceholder(){
-        //     // if (this.placeholder){
-        //     //     this.placeholder.parentNode.removeChild(this.placeholder);
-        //     // }
-        //     console.log(this.movingCandidate)
-        //     console.log("Placing Moving Candidate back into the list: " + this.movingCandidate);
-        //     this.ballot[this.placeholderIndex] = this.movingCandidate;
-        //     this.placeholder = null;
-        //     this.placeholderIndex = null;
-        // },
-        // mouseUpHandler() {
-        //     this.removePlaceholder();
-        //     this.isDragging = false;
-
-        //     // Remove the position styles
-        //     this.draggingEle.style.removeProperty('top');
-        //     this.draggingEle.style.removeProperty('left');
-        //     this.draggingEle.style.removeProperty('position');
-
-        //     this.x = null;
-        //     this.y = null;
-        //     this.draggingEle = null;
-
-        //     // Remove the handlers of `mousemove` and `mouseup`
-        //     document.removeEventListener('mousemove', this.mouseMoveHandler);
-        //     document.removeEventListener('mouseup', this.mouseUpHandler);
-        // },
-        // isAbove(nodeA, nodeB) {
-        //     if (nodeA != null && nodeB != null){
-        //         // Get the bounding rectangle of nodes
-        //         const rectA = nodeA.getBoundingClientRect();
-        //         const rectB = nodeB.getBoundingClientRect();
-
-        //         return (rectA.top + rectA.height / 2 < rectB.top + rectB.height / 2);
-        //     }
-        //     return false;
-        // },
     },
 };
 
-socket.on('updateResults', function(serverResults) {
-    app.updatePoll(serverResults);
+socket.on('listPolls', function(aListOfPollNames) {
+    console.log('Received Poll List');
+    app.setPollList(aListOfPollNames);
+});
+
+socket.on('providePoll', function(aPoll) {
+    console.log("received Poll:");
+    console.log(aPoll);
+    app.updatePoll(aPoll);
 });
 
 const app = Vue.createApp(vm).mount('#main');
--- server.js
+++ server.js
@@ -7,71 +7,94 @@ const socketio = require("socket.io");
 const io = socketio(server);
 app.use(express.static("pub"));
 
-let pollResult = [
-  'Audrey',
-  'Claire',
-  'Owen',
-  'Katie',
-  'Adam',
-  'Steve',
-  'Irving',
-  'Emma'
-];
-
-const submittedBallots = new Map();
-
-function compareCandidate(a, b) {
-    if (a[1] < b[1]) {
-        return 1;
+class Poll {
+    constructor(aName) {
+      this.name = aName;
+      this.pollResults = [];
+      this.submittedBallots = new Map();
     }
-    if (a[1] > b[1]) {
-        return -1;
+
+    compareCandidate(a, b) {
+        if (a[1] < b[1]) {
+            return 1;
+        }
+        if (a[1] > b[1]) {
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-}
 
-function calculatePoll(aUserId, aBallot) {
-    submittedBallots.set(aUserId, aBallot);
-    const ballotCount = new Map();
-
-    for(let [eachUser, eachBallot] of submittedBallots){
-        for(let i = 0; i < eachBallot.length; i++){
-            const eachCandidate = eachBallot[i];
-            const voteWeight = (0.5)**(i);    // weight =  ar^(n) where a = 1 and i = n - 1
-            if(ballotCount.has(eachCandidate)){
-                const newCandidateTotal = voteWeight + ballotCount.get(eachCandidate);
-                ballotCount.set(eachCandidate, newCandidateTotal);
-            } else {
-                ballotCount.set(eachCandidate, voteWeight);
+    calculatePoll(aUserId, aBallot) {
+        this.submittedBallots.set(aUserId, aBallot);
+        const ballotCount = new Map();
+    
+        for(let [eachUser, eachBallot] of this.submittedBallots){
+            for(let i = 0; i < eachBallot.length; i++){
+                const eachCandidate = eachBallot[i];
+                const voteWeight = (0.5)**(i);    // weight =  ar^(n) where a = 1 and i = n - 1
+                if(ballotCount.has(eachCandidate)){
+                    const newCandidateTotal = voteWeight + ballotCount.get(eachCandidate);
+                    ballotCount.set(eachCandidate, newCandidateTotal);
+                } else {
+                    ballotCount.set(eachCandidate, voteWeight);
+                }
             }
         }
-    }
-    console.log("Map of totals: ");
-    console.log(ballotCount);
-
-    const candidateTotals = Array.from(ballotCount.entries())
-        .sort(compareCandidate)
-        .map(eachCandidate => eachCandidate[0]);
+        console.log("Map of totals: ");
+        console.log(ballotCount);
     
-    console.log("Final Ordering: ");
-    console.log(candidateTotals);
-
-    pollResult = candidateTotals;
+        const candidateTotals = Array.from(ballotCount.entries())
+            .sort(this.compareCandidate)
+            .map(eachCandidate => eachCandidate[0]);
+        
+        console.log("Final Ordering: ");
+        console.log(candidateTotals);
+    
+        this.pollResults = candidateTotals;
+    }
 }
 
+const activePolls = new Map();
+
 io.on("connection", function(socket) {
-    console.log("Somebody connected.");
-    socket.emit('updateResults', pollResult);
+    console.log(`Voter ${socket.id} connected.`);
+    socket.emit('listPolls', Array.from(activePolls.keys()));
 
     socket.on("disconnect", function() {
-        console.log("Somebody disconnected.");
+        console.log(`Voter ${socket.id} disconnected.`);
     });
 
-    socket.on("submitBallot", function(aBallot) {
+    socket.on('proposePoll', function(aPollName) {
+        if (!activePolls.has(aPollName)){
+            activePolls.set(aPollName, new Poll(aPollName));
+            io.emit('listPolls', Array.from(activePolls.keys()));
+        }
+    });
+
+    socket.on('requestPoll', function(aPollName) {
+        console.log('Saw Request for Poll: ' + aPollName);
+        if(activePolls.has(aPollName)) {
+            socket.emit('providePoll', {
+                name: aPollName, 
+                results: activePolls.get(aPollName).pollResults
+            });
+            socket.leaveAll();
+            socket.join(aPollName);
+        }
+    });
+
+    socket.on("submitBallot", function(aPollName, aBallot) {
         console.log(`User: ${socket.id} submitted Ballot:`)
         console.log(aBallot);
-        calculatePoll(socket.id, aBallot);
-        io.emit('updateResults', pollResult);
+        if (activePolls.has(aPollName)){
+            const poll = activePolls.get(aPollName);
+            poll.calculatePoll(socket.id, aBallot);
+            console.log(poll.pollResults);
+            io.to(aPollName).emit('providePoll', {
+                name: aPollName, 
+                results: poll.pollResults
+            });
+        }
     });
 });
 
